baf473377f664b3f08e601f5373e9646bc7786af
Carla Serrão <104877365+CarlaSerraoOn@users.noreply.github.com>
2023-11-14 22:42:38 +0000

    comit Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import './App.css'
+import Card from "./components/Card/Card";
 
 function App() {
+  const item1 = {
+    name: "Rick Sanches",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+  }
+
+  const item2 = {
+    name: "Morty Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+  }
+
+  const item3 = {
+    name: "Summer Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+  }
+
+  const item4 = {
+    name: "Beth Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+  }
+
+  const items = [item1, item2, item3, item4]
+  
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <div className="tags">
-          <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137</div>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      
+     {items.map(function(element){
+        return <Card item={element} />
+     })}
     </>
   )
 }
 
-export default App
+export default App;
